2c5a1e539eecba27d036f1493b6a76499051374f
KonradKisiel <konrad.kisiel.webmaster@gmail.com>
2018-10-07 20:41:19 +0200

    Session

---
+++ WebDevelopment/07_Session/03_SignUp/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/satori/go.uuid"
+)
+
+type user struct {
+	UserName string
+	Password string
+	First    string
+	Last     string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}      // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", logged)
+	http.HandleFunc("/signup", signup)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	u := getUser(w, req)
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	u := getUser(w, req)
+	if !alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
+}
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	//process form submission
+	if req.Method == http.MethodPost {
+		//get form values
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+		f := req.FormValue("firstname")
+		l := req.FormValue("lastname")
+
+		//username taken?
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "Username already taken", http.StatusForbidden)
+			return
+		}
+
+		//create session
+		sID, err := uuid.NewV4()
+		if err != nil {
+			fmt.Println(err)
+		}
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+
+		//store user in dbUsers
+		u := user{un, p, f, l}
+		dbUsers[un] = u
+
+		//redirect
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+}
+
+func logged(w http.ResponseWriter, req *http.Request) {
+	//get cookie
+	c, err := req.Cookie("session")
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	un, ok := dbSessions[c.Value]
+	if !ok {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+	u := dbUsers[un]
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
+}
